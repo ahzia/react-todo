c0eb34a187589878536b189e78e11b0ec670f8a9
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-10-02 12:14:14 +0430

    Add a text input field and a submit button

---
+++ src/components/InputTodo.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Add Todo..." name="title" value={this.state.title} onChange={this.onChange} />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
