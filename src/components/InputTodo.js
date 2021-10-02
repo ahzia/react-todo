8d1f3e2663234834e594c51d5c867d8963ff504f
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-10-02 12:30:49 +0430

    Work on Update the Todos list

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -9,9 +9,16 @@ class InputTodo extends Component {
       [e.target.name]: e.target.value,
     });
   };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+      title: ""
+    });
+  };
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input type="text" placeholder="Add Todo..." name="title" value={this.state.title} onChange={this.onChange} />
         <button>Submit</button>
       </form>
